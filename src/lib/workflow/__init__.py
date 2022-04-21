15e07cb558618f08f495b52ee45aaa5035658790
Pavel Brodsky <pavel.brodsky@forter.com>
2022-04-21 23:25:22 +0300

    Solve importing issues

--- src/lib/workflow/__init__.py
+++ src/lib/workflow/__init__.py
@@ -11,6 +11,9 @@
 """A helper library for `Alfred <http://www.alfredapp.com/>`_ workflows."""
 
 import os
+import sys
+
+sys.path.append(os.path.dirname(os.path.realpath(__file__)))
 
 # Workflow objects
 from .workflow import Workflow, manager
